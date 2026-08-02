a7674777b56daa309ed2731bd7cf42ee67e06c71
Boobalan Arul <boobalanarul072007@gmail.com>
2026-08-02 18:33:00 +0530

    Add files via upload

---
+++ pattern_1.java
@@ -0,0 +1,16 @@
+public class pattern_1 {
+    public static void main(String[] args) {
+        int a =4;
+        for(int i=1;i<=a;i++){
+               for(int j=1;j<=a;j++){
+                System.out.print("*");
+               }
+               System.out.println(" ");
+        }
+    }
+}
+
+//****
+//****
+//****
+//****
